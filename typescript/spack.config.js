b6988f3217ba4bfbfa3e6db33886fb41ab0e5caa
めんたいまよ <102917864+mentai-mayo@users.noreply.github.com>
2024-06-09 18:40:51 +0900

    update spack.config.js
    察して♡

--- typescript/spack.config.js
+++ typescript/spack.config.js
@@ -3,7 +3,7 @@ const { resolve } = require('path');
 
 module.exports = config({
   entry: {
-    'iwara.tv.main': resolve(__dirname, 'scripts/main.ts'),
+    'main': resolve(__dirname, 'scripts/main.ts'),
   },
   output: {
     path: resolve(__dirname, 'scripts/'),
