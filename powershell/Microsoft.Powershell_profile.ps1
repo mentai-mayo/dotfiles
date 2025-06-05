95397c9bec66c530970665365c1613455f5f5a2e
めんたいまよ <102917864+mentai-mayo@users.noreply.github.com>
2025-06-06 05:01:36 +0900

    feat: update powershell profile
    見た目を変更したよ

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -4,19 +4,22 @@ function Prompt {
   # print "\n"
   Write-Host " "
   # print powershell version
-  Write-Host " Pwsh"("v" + $PSVersionTable.PSVersion.toString() + " ") -NoNewLine -BackgroundColor "Blue"
+  Write-Host ("(Pwsh v" + $PSVersionTable.PSVersion.toString() + ")") -NoNewLine -ForegroundColor "DarkYellow"
   # print current directory
-  Write-Host (" " + (Get-Location) + " ") -NoNewLine -BackgroundColor "DarkBlue"
+  Write-Host (" " + (Get-Location)) -NoNewLine -ForegroundColor "Cyan"
 
   # print git status
   if (Test-Path ".git") {
-    Write-Host (" " + (git rev-parse --abbrev-ref HEAD) + " ") -NoNewLine -BackgroundColor "Green" -ForegroundColor "Black"
+    Write-Host (" " + (git rev-parse --abbrev-ref HEAD)) -NoNewLine -ForegroundColor "Green"
   }
+
+  # print "\n"
+  Write-Host " "
 
   # print cursor
   if ( $? ) {
-    return "`e[32m> `e[0m"
+    return "`e[32m❯ `e[0m"
   } else {
-    return "`e[31m> `e[0m"
+    return "`e[31m❯ `e[0m"
   }
 }
